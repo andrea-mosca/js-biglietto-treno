ba9a4eddc497135498433d25b2fb308d3d075187
andrea-mosca <andrea.moscateli@gmail.com>
2025-04-01 14:33:33 +0200

    raccolta dati

--- js/main.js
+++ js/main.js
@@ -1,1 +1,14 @@
-console.log("hello")
+// Il programma dovrà chiedere all'utente il numero di chilometri che vuole percorrere e l'età del
+// passeggero.
+// Sulla base di queste informazioni dovrà calcolare il prezzo totale del viaggio, secondo queste
+// regole:
+// - il prezzo del biglietto è definito in base ai km (0.21 € al km)
+// - va applicato uno sconto del 20% per i minorenni
+// - va applicato uno sconto del 40% per gli over 65.
+// - L'output del prezzo finale va messo fuori in forma umana (con massimo due decimali, per indicare
+//   centesimi sul prezzo).
+
+// ----RACCOLTA DATI----
+const utentKm = parseInt(prompt("inserisci il numero di Km da percorrere", "50"));
+const utentAge = parseInt(prompt("inserisci la tua età", "18"));
+const eurForKm = utentKm * 0.21;
